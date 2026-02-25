6ff907e085864db0beaba1d2b0f536463c4711d5
SimonovBlonsky <995160396@qq.com>
2026-02-25 22:39:22 +0800

    Add Hesai Pandar

--- include/preprocess.h
+++ include/preprocess.h
@@ -115,6 +115,39 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(xt32_ros::Point,
                                   (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(double, timestamp, timestamp)(std::uint16_t, ring, ring))
 /*****************/
 
+/*** Hesai_Pandar40P ***/
+namespace Pandar40P_ros
+{
+struct EIGEN_ALIGN16 Point
+{
+  PCL_ADD_POINT4D;
+  float intensity;
+  float t;
+  std::uint16_t ring;
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
+} // namespace xt40_ros
+POINT_CLOUD_REGISTER_POINT_STRUCT(Pandar40P_ros::Point,
+                                  (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(float, t, t)(std::uint16_t, ring, ring))
+/*****************/
+// namespace Pandar40P_ros
+// {
+// struct EIGEN_ALIGN16 Point
+// {
+//   PCL_ADD_POINT4D;
+//   float intensity;
+//   std::uint32_t t;
+//   std::uint16_t reflectivity;
+//   uint8_t ring;
+//   std::uint16_t ambient;
+//   std::uint32_t range;
+//   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+// };
+// } // namespace ouster_ros
+// POINT_CLOUD_REGISTER_POINT_STRUCT(Pandar40P_ros::Point, (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)
+//                                   (std::uint32_t, t, t)(std::uint16_t, reflectivity,
+//                                                         reflectivity)(std::uint8_t, ring, ring)(std::uint16_t, ambient, ambient)(std::uint32_t, range, range))
+
 /*** Hesai_Pandar128 ***/
 namespace Pandar128_ros
 {
@@ -173,6 +206,7 @@ private:
   void avia_handler(const livox_ros_driver::CustomMsg::ConstPtr &msg);
   void oust64_handler(const sensor_msgs::PointCloud2::ConstPtr &msg);
   void velodyne_handler(const sensor_msgs::PointCloud2::ConstPtr &msg);
+  void Pandar40P_handler(const sensor_msgs::PointCloud2::ConstPtr &msg);
   void xt32_handler(const sensor_msgs::PointCloud2::ConstPtr &msg);
   void Pandar128_handler(const sensor_msgs::PointCloud2::ConstPtr &msg);
   void robosense_handler(const sensor_msgs::PointCloud2::ConstPtr &msg);
